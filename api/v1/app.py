3d1942ee184f0fedd0ce253529c5348ca8e3f210
reem <tarekreem950@gmail.com>
2023-10-30 15:32:09 -0700

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,9 +8,11 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown(var):
     storage.close()
+
 
 if __name__ == '__main__':
     host = '0.0.0.0'
