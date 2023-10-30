2319ade31d0450b29b20f5070e921c887d0923a9
reem <tarekreem950@gmail.com>
2023-10-30 14:55:31 -0700

    api

---
+++ api/v1/views/index.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""return json"""
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', methods=['GET'])
+def stat():
+    return jsonify({'status': 'OK'})
