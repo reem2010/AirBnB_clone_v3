f861786787b543fb9f26da8c147caa2f19c16235
reem <tarekreem950@gmail.com>
2023-10-30 16:17:50 -0700

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """the app file"""
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 import os
@@ -13,6 +13,10 @@ app.register_blueprint(app_views)
 def teardown(var):
     storage.close()
 
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
     host = '0.0.0.0'
