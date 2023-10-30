9ed51bd37f0393f396b53b42c096b8e99312911e
reem <tarekreem950@gmail.com>
2023-10-30 15:51:19 -0700

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,28 @@
 """return json"""
 from api.v1.views import app_views
 from flask import jsonify
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from models import storage
 
 
 @app_views.route('/status')
 def status():
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats')
+def stats():
+    res = {
+            "menities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)
+            }
+    return jsonify(res)
